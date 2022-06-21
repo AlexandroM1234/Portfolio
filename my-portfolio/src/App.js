e76870c41c9108cae550e2059573cb70c95084c7
Alex Martinez <alexmartinez0071234@gmail.com>
2022-06-20 20:09:46 -0400

    Inital file setup done

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex justify-center underline">
+      <h1 className="text-6xl font-bold">Hello World!</h1>
+    </main>
+  );
+}
+
+export default App;
