2cd98cebb32cd43a2df56ed2b5ccab7287dbe5b4
Alex Martinez <alexmartinez0071234@gmail.com>
2022-06-23 15:58:23 -0400

    Projects data added

--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -14,3 +14,29 @@ export const skills = [
   "Cypress",
   "Git",
 ];
+
+export const projects = [
+  {
+    title: "EnCon",
+    subtile: "React",
+    description: "EnCon lets users see and track their energy usage.",
+    image: "",
+    link: "https://master.d3dr1o3e9t60pz.amplifyapp.com/",
+  },
+  {
+    title: "How To",
+    subtile: "React & Redux",
+    description:
+      "How-To is an application that acts as an information form and board to post How-To guides for a variety of activities ranging from tech to D.I.Y. projects.",
+    image: "",
+    link: "https://front-end-ruddy.vercel.app/",
+  },
+  {
+    title: "Expat Journal API",
+    subtile: "Express & SQL",
+    description:
+      "An API and database built for a team of front-end engineers to receive images and user data. ",
+    image: "",
+    link: "https://expat-journal-server.herokuapp.com/",
+  },
+];
